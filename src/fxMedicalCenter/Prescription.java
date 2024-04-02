f8545b472050cf9048118232746631592b14893d
IamUncatchable <80870683+IamUncatchable@users.noreply.github.com>
2024-04-02 11:53:54 -0700

    Enums and Prescription

---
+++ src/fxMedicalCenter/Prescription.java
@@ -0,0 +1,47 @@
+package fxMedicalCenter;
+
+import java.time.LocalDate;
+
+public class Prescription {
+	int id;
+	LocalDate date;
+	int refills;
+	String prescribed;
+	
+	public Prescription(int prescriptID,LocalDate newDate,int numRefills,String prescription){
+		id = prescriptID;
+		date = newDate;
+		refills = numRefills;
+		prescribed = prescription;
+		createPrescription();
+	}
+	
+	public Prescription(int prescriptID) {
+		id = prescriptID;
+		retrievePrescription();
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public int getRefills() {
+		return refills;
+	}
+	
+	public String getPrescription() {
+		return prescribed;
+	}
+
+	private void createPrescription() {
+		Database database = new Database();
+	}
+	
+	private void retrievePrescription() {
+		Database database = new Database();
+	}
+}
