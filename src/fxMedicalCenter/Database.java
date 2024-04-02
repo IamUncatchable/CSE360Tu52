4f27a85598da93631b9f70719974238d9ee2f7d4
IamUncatchable <80870683+IamUncatchable@users.noreply.github.com>
2024-04-02 12:49:20 -0700

    Database Enums

--- src/fxMedicalCenter/Database.java
+++ src/fxMedicalCenter/Database.java
@@ -9,6 +9,70 @@ public class Database {
 	private ResultSet rs;
 	private String query;
 
+	public enum Datatables{
+		VISIT("visit"),
+		MESSAGE("message"),
+		USERS("users"),
+		PRESCRIPTION("prescription"),
+		PATIENT("patient");
+		
+		
+		private final String dataTableName;
+
+		 Datatables(String dataTableName) {
+				this.dataTableName = dataTableName;
+			}
+			
+			public String get() {
+				return dataTableName;
+			}
+	}
+	
+	public enum Columns {
+		
+		MESSAGE("message"),
+		FROM("from"),
+		TO("to"),
+		DATE("date"),
+		TEXT("text"),
+		PATIENT_ID("patient_id"),
+		GENDER("gender"),
+		ADDRESS("address"),
+		CITY("city"),
+		STATE("state"),
+		ZIP("zip"),
+		PHONE_NUMBER("phone_number"),
+		EMAIL("email"),
+		INSURANCE_PROVIDER("insurance_provider"),
+		INSURANCE_ID("insurance_id"),
+		HISTORY("history"),
+		FIRST_NAME("first_name"),
+		LAST_NAME("last_name"),
+		PRESCRIPTION_ID("prescription_id"),
+		REFILLS("refills"),
+		PRESCRIBED("prescribed"),
+		USERNAME("username"),
+		BIRTHDAY("birthday"),
+		PASSWORD("password"),
+		ACCOUNT_TYPE("account_type"),
+		VISIT_ID("visit_id"),
+		HEIGHT("height"),
+		WEIGHT("weight"),
+		TEMP("temp"),
+		BLOOD_PRESSURE("blood_pressure"),
+		NURSE_NOTES("nurse_notes"),
+		DR_NOTES("dr_notes");
+		
+		private final String columnName;
+
+		 Columns(String columnName) {
+				this.columnName = columnName;
+			}
+			
+			public String get() {
+				return columnName;
+			}
+	}
 	
 	public Database() {
 		databaseconnect();
