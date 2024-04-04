2a410869b11732095a1d0fc56e7b9c76104de0c1
SprocketSurgeon <164565235+SprocketSurgeon@users.noreply.github.com>
2024-04-04 07:07:28 -0700

    Update DashboardTemplate.java

--- src/fxMedicalCenter/DashboardTemplate.java
+++ src/fxMedicalCenter/DashboardTemplate.java
@@ -1,12 +1,12 @@
 package fxMedicalCenter;
 //Constructor initializes screen, call getView to get the screen
-// still need to add content for all of the dashboard screens.
-// it's big and ugly and I am sorry
+//screen size is 1300 x 780
 //
-//still need to add button actions
+
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -23,37 +23,29 @@ import javafx.scene.shape.Line;
 
 public class DashboardTemplate {
 	
-	private BorderPane screen; // Root node of this  view
-	private GridPane grid;
-	private ColumnConstraints col0, col1, col2, col3, col4, col5, col6;
-	private RowConstraints row0, row1, row2, row3, row4, row5;
-	private VBox leftBar; //vbox for left sidebar
-	private VBox imageBox;
-	private Button SignOutButton;
-	private Button homeButton;
-	private HBox topLeftTabs;
-	private Pane rightSide;
-	
+	private BorderPane screen;
+  private GridPane grid;
+  private VBox leftBar;
+  private HBox topLeftTabs;
+  private static final String[] MENU_ITEMS = {"Dashboard", "My Account", "Messages", "Medical Records", "Appointments"};
+  private static final String LIGHT_GREEN = "-fx-background-color: #aedd94;";
+  private static final String DARK_GREEN = "-fx-background-color: #629c44;";
+  private static final String OFF_WHITE = "-fx-background-color: #F7F6F6;";
+  private static final String DASH_BANNER_FONT = "-fx-font-size: 24px;";
 	//constructor
 	public DashboardTemplate() {
 		   screen = new BorderPane();
 		   initializeScreen();
 	}
-	
-	//text for for left sidebar
-	private static final String[] MENU_ITEMS = {
-			"Dashboard", "My Account", "Messages", "Medical Records",
-			" Appointments"
-	};
-	
+			
 	//buttons for left side bar
 	private Button createButton(String text) {
-		Button leftBarButton = new Button(text);
-		leftBarButton.setMaxWidth(Double.MAX_VALUE);
-		leftBarButton.setOnAction(e -> setActivePage(text));
-		leftBarButton.setStyle("-fx-background-color: #aedd94;");//light green
-		return leftBarButton;
-	}
+      Button button = new Button(text);
+      button.setMaxWidth(Double.MAX_VALUE);
+      button.setOnAction(e -> setActivePage(text));
+      button.setStyle(LIGHT_GREEN);
+      return button;
+  }
 	
 	//function to move to other pages
 	private void setActivePage(String page) {
@@ -62,9 +54,9 @@ public class DashboardTemplate {
 	
 		for (Button leftBarButton : leftBar.getChildren().stream().map(n -> (Button) n).toList()) {
 	        if (leftBarButton.getText().equals(page)) {
-	            leftBarButton.setStyle("-fx-background-color: #629c44;");//dark green
+	            leftBarButton.setStyle(DARK_GREEN);//dark green
 	        } else {
-	            leftBarButton.setStyle("-fx-background-color: #aedd94;");//light green
+	            leftBarButton.setStyle(LIGHT_GREEN);//light green
 	        }
 		}
 	}
@@ -76,26 +68,23 @@ public class DashboardTemplate {
 		// work on only the main elements of the 
 		//dash view that will stay the same on every page
 		grid = new GridPane();
-		leftBar = new VBox();
-		topLeftTabs = new HBox();
+      leftBar = new VBox(5);
+      topLeftTabs = new HBox();
 		
 		// page background color #F7F6F6
-		screen.setStyle("-fx-background-color: #F7F6F6;");
+      screen.setStyle(OFF_WHITE);
 		
 		// add logo image
 		// update path or place on web for access
-		// go in grid 0,0, 1, 3 row 0 column 0 occupy 1 column and 3 rows
 		// add resources folder to build path
-		// /Minimalist_Hospital_and_Medical_Health_Logo.png
-		//FIXME image not aligned right need to adjust
-		Image image = new Image("/Minimalist_Hospital_and_Medical_Health_Logo.png");
-		ImageView logoView = new ImageView(image);
+		// Minimalist_Hospital_and_Medical_Health_Logo.png
+		// FIXME image not aligned right need to adjust
+		ImageView logoView = new ImageView(new Image("/Minimalist_Hospital_and_Medical_Health_Logo.png"));
 		logoView.setFitHeight(160);
 		logoView.setFitWidth(160);
 		logoView.setPreserveRatio(true);
 		//place image in container
-		imageBox = new VBox(logoView);
-		imageBox.setAlignment(Pos.BOTTOM_CENTER);
+		GridPane.setValignment(logoView, VPos.BOTTOM); //FIXME not having desired effect
 		
 		//left sidebar vbox will go in (0,1) column 0, row 1		
 		leftBar.setSpacing(5);;
@@ -107,78 +96,45 @@ public class DashboardTemplate {
 		}
 		
 		
-		
 		// create tabs for home and logout
-		// place buttons in grid pane position 2,0,2,1
-		SignOutButton = new Button("Sign Out");
-		homeButton = new Button("Home");
-		
-		//styling for home and signout tabs
-		SignOutButton.setStyle("-fx-background-color: #aedd94; -fx-background-radius: 15 15 0 0;");//light green
-		SignOutButton.setPrefWidth(215);
-		homeButton.setStyle("-fx-background-color: #aedd94; -fx-background-radius: 15 15 0 0;");
+		Button signOutButton = new Button("Sign Out"), homeButton = new Button("Home");
+      signOutButton.setStyle(LIGHT_GREEN + " -fx-background-radius: 15 15 0 0;");
+		signOutButton.setPrefWidth(215);
+      homeButton.setStyle(LIGHT_GREEN + " -fx-background-radius: 15 15 0 0;");
 		homeButton.setPrefWidth(215);
-		
-		//place buttons in container
-		topLeftTabs.getChildren().addAll(homeButton, SignOutButton);
-		
+      topLeftTabs.getChildren().addAll(homeButton, signOutButton);
+
+				
 		//add side art
-		// place in grid pane at 4, 0, 0, 5
-		Image sideArt = new Image("/rightBar1.jpg");
-		ImageView sideArtView = new ImageView(sideArt);
+      ImageView sideArtView = new ImageView(new Image("/rightBar1.jpg"));
 		sideArtView.setFitHeight(700);
 		sideArtView.setFitWidth(215);
 		sideArtView.setPreserveRatio(true);
 		
-		//place image in container
-		rightSide = new Pane(sideArtView);
+		
 		
 		//align top left tabs
 		GridPane.setHalignment(topLeftTabs, HPos.RIGHT);
 		
+		//set up column and row constraints
+		
+		
 		// add page banner label
 		Label bannerLabel = new Label("This is different on every page");
-		bannerLabel.setStyle("-fx-font-size: 24px;");
+		bannerLabel.setStyle(DASH_BANNER_FONT);
 		Line bannerStylingLine = new Line(0, 0, 400, 0);
-		bannerStylingLine.setStyle("-fx-stroke: #aedd94;");
+		bannerStylingLine.setStyle(LIGHT_GREEN);
 		VBox bannerBox = new VBox(10);
 		bannerBox.setAlignment(Pos.CENTER_LEFT);
 		bannerBox.getChildren().addAll(bannerLabel,bannerStylingLine );
 	
-		
-		
-		
-		
-		//column constraints
-		col0 = new ColumnConstraints(30);
-		col1 = new ColumnConstraints(160);
-		col2 = new ColumnConstraints(50);
-		col3 = new ColumnConstraints(240);
-		col4 = new ColumnConstraints(240);
-		col5 = new ColumnConstraints(240);
-		col6 = new ColumnConstraints(250);
-		
-		//set row constraints 60,60,60,230,230
-		// rows beyond 2 will need to change to accomodate specific screens
-		//this arrangement is for card display 
-		// text field and label displays require smaller grid panes height 40
-		// would love to have dynamic resizing but...
-		row0 = new RowConstraints(50);
-		row1 = new RowConstraints(60);
-		row2 = new RowConstraints(60);
-		row3 = new RowConstraints(60);
-		row4 = new RowConstraints(230);
-		row5 = new RowConstraints(230);
-		
+				
 		//apply padding and column constraints for gridpane
-		grid.setPadding(new Insets(20,20,20,20));
-		grid.getColumnConstraints().addAll(col0, col1, col2, col3, col4, col5, col6);
-		grid.getRowConstraints().addAll(row0, row1, row2, row3, row4, row5);
-		
+		setupGridPane();
 		//add left side bar
 		grid.add(leftBar, 1, 4);
 		//add image
-		grid.add(imageBox, 1, 1, 1, 3);
+		grid.add(logoView, 1, 1, 1, 3);
 		//add top left tabs
 		grid.add(topLeftTabs, 4, 1, 2, 1);
 		//add right side art
@@ -189,6 +145,13 @@ public class DashboardTemplate {
 		screen.setCenter(grid);
 		
 	}
+	 private void setupGridPane() {
+	        grid.setPadding(new Insets(20));
+	        grid.getColumnConstraints().addAll(new ColumnConstraints(60), new ColumnConstraints(160), new ColumnConstraints(50),
+	                new ColumnConstraints(240), new ColumnConstraints(240), new ColumnConstraints(240), new ColumnConstraints(250));
+	        grid.getRowConstraints().addAll(new RowConstraints(50), new RowConstraints(60), new RowConstraints(60),
+	                new RowConstraints(60), new RowConstraints(230), new RowConstraints(230));
+	    }
 	
 	public Parent getView() {
 		   return screen;
